81217a3d8fa87c53d37755ce3e4442a6af73dae0
Duy <duy.tonghai@unified.com>
2019-06-03 14:44:21 +0700

    Clean code

--- background.js
+++ background.js
@@ -3,29 +3,34 @@
 // found in the LICENSE file.
 'use strict';
 
-var BASE_URL = 'https://go.isclix.com/deep_link/5140210465117321985?url=';
+const ACCESS_TRADE_KEY = '5140210465117321985';
+const  BASE_URL = `https://go.isclix.com/deep_link/${ACCESS_TRADE_KEY}?url=`;
+
+function verifyPublisher(requestUrl) {
+  const publishers = [
+    'tiki.vn/',
+    'shopee.vn/',
+    'sendo.vn/',
+    'adayroi.com/',
+    'fado.vn'
+  ];
+
+  return publishers.find(item => requestUrl.includes(item)) ? true : false;
+}
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log('Working...');
   });
 
   // onUpdated should fire when the selected tab is changed or a link is clicked
-  var myUrl;
   chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     chrome.tabs.getSelected(null, function(tab) {
-      myUrl = tab.url || '';
+      var myUrl = tab.url || '';
 
-      var isValidPublisher = false;
-      var publishers = ['tiki.vn/', 'shopee.vn/', 'sendo.vn/', 'adayroi.com/', 'fado.vn'];
-      publishers.forEach(function(item) {
-        if (myUrl.includes(item)) {
-          isValidPublisher = true;
-          return;
-        }
-      });
-
+      const isValidPublisher = verifyPublisher(myUrl);
       if (isValidPublisher) {
-        var accessTradeUrl = `${BASE_URL}${myUrl}&utm_source=accesstrade`;
+        const  accessTradeUrl = `${BASE_URL}${myUrl}&utm_source=accesstrade`;
         console.log(">>>>>>>>>>>>>>>>>>>>>>>", accessTradeUrl);
 
         window.location.replace(accessTradeUrl);
