6f6483230a95a41eadb737ee23179861a331763c
Duy <duy.tonghai@unified.com>
2019-06-04 17:04:50 +0700

    fix code

--- background.js
+++ background.js
@@ -18,10 +18,15 @@ function verifyPublisher(requestUrl) {
   return publishers.find(item => requestUrl.includes(item)) ? true : false;
 }
 
+function resetOldUrl() {
+  
+}
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log('Working...');
   });
+  var oldMyUrl = '';
 
   // onUpdated should fire when the selected tab is changed or a link is clicked
   chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
@@ -29,14 +34,27 @@ chrome.runtime.onInstalled.addListener(function() {
       var myUrl = tab.url || '';
 
       console.log("myURL>>>>>>>>>>>>>>>>>>>>>>>>>>>>", myUrl);
-
       const isValidPublisher = verifyPublisher(myUrl);
       if (isValidPublisher) {
         const myUrlEncoded = encodeURIComponent(myUrl);
         const accessTradeUrl = `${BASE_URL}${myUrlEncoded}`;
         console.log(">>>>>>>>>>>>>>>>>>>>>>>", accessTradeUrl);
 
-        window.location.href = accessTradeUrl;
+        /**
+         * Only open tab when
+         * * oldMyUrl is '' (first time)
+         * * 
+         */
+
+        if (oldMyUrl === '' || (myUrl !== oldMyUrl)) {
+          // var timeStamp = new Date().valueOf();
+          // oldMyUrl = `${myUrl}_${timeStamp}`;
+          window.open(accessTradeUrl);
+          var timestamp = new Date().valueOf();
+          oldMyUrl = myUrl + '__' + timestamp;
+        } else {
+          oldMyUrl = myUrl + '__' + timestamp;
+        }
       }
     });
   });
