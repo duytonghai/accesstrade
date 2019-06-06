0491b5228f23fffc72afd7466f3b948c5c521c85
Duy <duy.tonghai@unified.com>
2019-06-06 09:17:38 +0700

    Add feature insert key & auto clear cookies every week

--- background.js
+++ background.js
@@ -3,10 +3,11 @@
 // found in the LICENSE file.
 'use strict';
 
+const ONE_WEEK_BY_SECONDS = 604800000;
 const LOCAL_STORAGE_KEY = 'links';
-const ACCESS_TRADE_KEY_DITO = '5140210465117321985';
-const ACCESS_TRADE_KEY = '4898793259950129977';
-const BASE_URL = `https://go.isclix.com/deep_link/${ACCESS_TRADE_KEY}?url=`;
+const LOCAL_STORAGE_ACCESS_TRADE_KEY = 'accessTradeKey';
+const ACCESS_TRADE_KEY = '5140210465117321985';
+const BASE_URL = `https://go.isclix.com/deep_link/`;
 
 const PUBLISHERS = [
   'tiki.vn',
@@ -50,9 +51,34 @@ function setStorage(newLink) {
   return;
 }
 
+// Clear all storage data every week to refresh access trade cookies
+function cleanData() {
+  setInterval(() => {
+    console.log('Cleaning cookies every week...');
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+  }, ONE_WEEK_BY_SECONDS);
+  return;
+}
+
+function initKey() {
+  const accessPrivate = getStorage(LOCAL_STORAGE_ACCESS_TRADE_KEY);
+  if (!accessPrivate || !accessPrivate.length) {
+    let key = prompt('Please insert your access trade key');
+    if (!key) {
+      key = ACCESS_TRADE_KEY;
+    }
+    localStorage.setItem(LOCAL_STORAGE_ACCESS_TRADE_KEY, key);
+    
+    alert(`Your access trade key ${key} has been setted`);
+  }
+  return;
+}
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
     console.log('Working...');
+    initKey();
+    cleanData();
   });
 
   // onUpdated should fire when the selected tab is changed or a link is clicked
@@ -63,7 +89,8 @@ chrome.runtime.onInstalled.addListener(function() {
       const isValidPublisher = verifyPublisher(newLink);
       if (isValidPublisher) {
         const newLinkEncoded = encodeURIComponent(newLink);
-        const accessTradeUrl = `${BASE_URL}${newLinkEncoded}`;
+        const accessPrivate = localStorage.getItem(LOCAL_STORAGE_ACCESS_TRADE_KEY);
+        const accessTradeUrl = `${BASE_URL}${accessPrivate}?url=${newLinkEncoded}`;
         console.log(">>>>>>>>>>>>>>>>>>>>>>>", accessTradeUrl);
 
         var linkArray = getStorage(LOCAL_STORAGE_KEY);
